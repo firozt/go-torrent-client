224e840886260e9f8a94802a5bc7e0d660bc1601
firozt <firozt03@gmail.com>
2026-02-09 23:14:38 +0000

    working state

--- src/internal/TorrentClient/TorrentClient.go
+++ src/internal/TorrentClient/TorrentClient.go
@@ -1,1 +1,14 @@
 package torrentclient
+
+import (
+	torrent "github.com/firozt/go-torrent/src/internal/Torrent"
+)
+
+type TorrentClient struct {
+}
+
+func (t *TorrentClient) StartTorrent(torrentData torrent.Torrent) {
+
+}
+
+// func (t *TorrentClient) getTrackers
